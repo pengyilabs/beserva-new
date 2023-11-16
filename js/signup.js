9ff4d7e2ed683c70abbcd974a9ada37d8c937c27
dayana.loaiza <dayanalr1992@gmail.com>
2023-11-15 18:33:57 -0600

    general fix on onboarding page

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,3 @@
-
 const progressSteps = document.querySelectorAll('.progress-step');
 const forms = document.querySelectorAll('.form-step');
 const serviceForm = document.querySelector('#service-form');
@@ -6,8 +5,12 @@ const teamMemberForm = document.querySelector('#team-member-form');
 const backButton = document.querySelector('#back-btn');
 const saveProfileButton = document.querySelector('#save-profile-btn');
 const nextButton = document.querySelector('#next-btn');
+const services = document.querySelectorAll('service-item');
+
 
 let FORM_NUMBER = 0;
+const SERVICES = [];
+const TEAM_MEMBERS = [];
 
 FORM_NUMBER === 0 ? backButton.classList.add('d-none') : backButton.classList.remove('d-none');
 
@@ -39,21 +42,83 @@ const updateProgressSteps = () => {
 const addNewService = () => {
   serviceForm.classList.remove('d-none');
   document.getElementById('new-service-btn').classList.add('d-none');
+  document.getElementById('services-list').classList.add('d-none');
 }
 
 const saveService = () => {
+  const category = document.getElementById('service-category').value;
+  const name = document.getElementById('service-name').value;
+  const price = document.getElementById('service-price').value;
+
   serviceForm.classList.add('d-none');
   document.getElementById('new-service-btn').classList.remove('d-none');
+  document.getElementById('services-list').classList.remove('d-none');
+
+  const serviceItem = 
+    `<div class="service-item card mb-2 border-secondary" id="${services.length}">
+      <div class="card-body d-flex justify-content-between">
+        <div>
+          <span class="fs-7 text-gray">${category}</span>
+          <h6 class="mb-0">${name}</h6>
+          <span class="text-gray">${price}</span>
+        </div>
+        <div>
+          <button class="btn btn-link">
+            <i class="bi bi-pen" style="color: $gray"></i>
+          </button>
+          <button class="btn btn-link">
+            <i class="bi bi-trash" style="color: $gray"></i>
+          </button>
+        </div>
+      </div>
+    </div>`
+
+  SERVICES.push(serviceItem);
+  const servicesString = SERVICES.join(',').replace(',' , '');
+  document.getElementById('services-list').innerHTML = servicesString;
 }
 
 const addNewTeamMember = () => {
   teamMemberForm.classList.remove('d-none');
   document.getElementById('new-team-member-btn').classList.add('d-none');
+  document.getElementById('members-list').classList.add('d-none');
 }
 
 const saveTeamMember = () => {
+  const category = document.getElementById('member-category').value;
+  const name = document.getElementById('member-name').value;
+
   teamMemberForm.classList.add('d-none');
-  document.getElementById('save-team-member-btn').classList.remove('d-none');
+  document.getElementById('new-team-member-btn').classList.remove('d-none');
+  document.getElementById('members-list').classList.remove('d-none');
+
+  const memberItem = 
+  `<div class="member-item card mb-2 border-secondary" id="${services.length}">
+    <div class="card-body d-flex justify-content-between">
+      <div class="d-flex">
+        <div class="rounded-circle>
+          <img src='assets/images/profile/review-profile-photo.png'>
+        </div>
+        <div>
+          <span class="fs-7 text-gray">${category}</span>
+          <h6 class="mb-0">${name}</h6>
+          <span class="text-success fs-7">3 services</span>
+        </div>
+      </div>
+      <div>
+        <button class="btn btn-link">
+          <i class="bi bi-pen" style="color: $gray"></i>
+        </button>
+        <button class="btn btn-link">
+          <i class="bi bi-trash" style="color: $gray"></i>
+        </button>
+      </div>
+    </div>
+  </div>`
+
+  TEAM_MEMBERS.push(memberItem);
+  const memberString = TEAM_MEMBERS.join(',').replace(',' , '');
+  document.getElementById('members-list').innerHTML = memberString;
 }
 
 const saveProfile = () => {
